4db8640c3b6207eb1a8bea88565075cccb7f2a6b
Matt Burnett <matt.burnett@abcfinancial.com>
2017-10-10 20:30:32 -0500

    build requester
    KINDA PROUD OF THIS ONE!

--- src/processOrders.mjs
+++ src/processOrders.mjs
@@ -1,47 +1,52 @@
 import rimraf from 'rimraf'
 import moment from 'moment'
+import requester from './requester'
 import RegionMarket from './RegionMarket'
 import ItemOrders from './ItemOrders'
 import empire from '../eveData/empireRegions.json'
-const outputDir = './tmp/items'
+
+const processMarketData = (data, oldData) => {
+    const newData = oldData
+    const marketData = [].concat(...data)
+
+    marketData.forEach(order => {
+        let itemOrder = newData[order.type] || new ItemOrders(order.type)
+
+        if(order.buy) {
+            itemOrder.orders.buy.push(order)
+        } else {
+            itemOrder.orders.sell.push(order)
+        }
+
+        newData[order.type] = itemOrder
+    })
+
+    return newData
+}
+
+const getRegionData = () => {
+    return requester({
+        chain: empire,
+        data: {},
+        requester: (r) => new RegionMarket(r).getData(),
+        processor: processMarketData,
+        limit: 3,
+    })
+}
 
 const processOrders = () => {
-    return new Promise((resolve, reject) => {
-        console.log('getting items...')
-        const empireMarket = empire.map(region => new RegionMarket(region).getData())
+    return getRegionData()
+        .then(data => {
+            const items = {
+                expires: moment().add(300, 'seconds').utc().format()
+            }
 
-        Promise.all(empireMarket)
-            .then(data => {
-                const marketData = [].concat(...data)
-                console.log(`Got ${marketData.length} items!`)
-                
-                const items = {
-                    expires: moment().add(300, seconds).utc().format()
-                }
-
-                marketData.forEach(order => {
-                    let itemOrder = items[order.type] || new ItemOrders(order.type)
-
-                    if(order.buy) {
-                        itemOrder.orders.buy.push(order)
-                    } else {
-                        itemOrder.orders.sell.push(order)
-                    }
-
-                    items[order.type] = itemOrder
-                })
-
-                const finalItems = {}
-
-                Object.keys(items).forEach(key => {
-                    finalItems[key] = ItemOrders.filterByProfit(items[key], 10)     
-                })
-
-                resolve(finalItems)
-            }).catch(err => {
-                reject(err)
+            Object.keys(data).forEach(key => {
+                items[key] = ItemOrders.filterByProfit(data[key], 10)
             })
-    })
+
+            return items
+        })
 }
 
 export default processOrders
---
+++ src/requester.mjs
@@ -0,0 +1,56 @@
+const requester = (input) => {
+    return new Promise((resolve, reject) => {
+        const chain = input.chain
+        const limit = input.limit
+        const data = input.data
+        const requester = input.requester
+        const processor = input.processor
+
+        const chainLen = chain.length < limit ? chain.length : limit
+
+        const shortChain = []
+
+        for(let i = 0; i < chainLen; ++i) {
+            shortChain.push(requester(chain.pop()))
+        }
+
+        Promise.all(shortChain)
+            .then(resolveData => {
+                const newData = processor(resolveData, data)
+
+                resolve({
+                    chain,
+                    limit,
+                    data: newData,
+                    requester,
+                    processor,
+                })
+            })
+            .catch(err => {
+                console.log(`retrying ${JSON.stringify(shortChain)}`)
+
+                let retry = input.retry || 10
+
+                if(retry === 0) {
+                    reject(new Error('Requester tried too many times'))
+                }
+
+                resolve({
+                    chain,
+                    limit,
+                    data,
+                    requester,
+                    processor,
+                    retry: --count,
+                })
+            })
+    }).then(data => {
+        if(data.chain.length === 0) {
+            return data.data
+        }
+
+        return requester(data)
+    })
+}
+
+export default requester
