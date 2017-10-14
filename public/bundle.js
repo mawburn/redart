2143d8f809443991deb15a5b2c0ccee3a7b20751
matt burnett <mawburn@users.noreply.github.com>
2017-10-13 21:28:40 -0500

    holy crap

--- public/bundle.js
+++ public/bundle.js
@@ -2,16 +2,15 @@ const app = document.getElementById('app')
 
 let count = 0
 
-const getOrders = () => {
-  return fetch('./orders')
-    .then(res => {
-      if(res.statusCode !== 200 && res.statusCode < 300) {
-        return undefined
-      }
-      return res.json()
-    })
-    .catch(err => console.error(err))
-}
+const getOrders = () => fetch('./orders')
+  .then(res => {
+    if(res.statusCode !== 200 && res.statusCode < 300) {
+      return undefined
+    }
+    return res.json()
+  })
+  .catch(err => console.error(err))
+
 
 const printOrders = () => {
   ++count
@@ -21,19 +20,17 @@ const printOrders = () => {
     .then(orders => {
       const items = orders.items
 
-      if(!items && orders.pending) {
+      if(orders.pending) {
         app.innerHTML = `<div style="text-align:center">Waiting: ${count}</div>`
         setTimeout(printOrders, 1000)
       } else {
-        app.innerHTML = `Total Items: ${Object.keys(items).length - 2}`
-
         Object.keys(items).forEach(key => {
           if(key !== 'expires' && key !== 'pending') {
-            const item = items.key
-            output.push(`<div style="padding:0.5rem"><strong>${item.name}</strong> : Vol (${item.mass}) : Sell (${item.orders.sell.length}) : Buy (${item.orders.buy.length})</div>`)
+            const item = items[key]
+            output.push(`<div style="padding:0.5rem"><strong>${item.name}</strong> : Vol (${item.vol}) : Sell (${item.orders.sell.length}) : Buy (${item.orders.buy.length})</div>`)
           }
         })
-        app.innerHTML = app.innerHTML + output.join('')
+        app.innerHTML = output.join('')
       }
     }).catch(err => console.error(err))
 }
