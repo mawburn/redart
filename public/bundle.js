c34fdb0939b84834ee251f0844f50156cc344100
matt <mawburn7@gmail.com>
2017-10-13 20:13:11 -0500

    small gui change
    gitkraken is really messing up my commits

--- public/bundle.js
+++ public/bundle.js
@@ -2,15 +2,16 @@ const app = document.getElementById('app')
 
 let count = 0
 
-const getOrders = () => fetch('./orders')
-  .then(res => {
-    if(res.statusCode !== 200 && res.statusCode < 300) {
-      return undefined
-    }
-    return res.json()
-  })
-  .catch(err => console.error(err))
-
+const getOrders = () => {
+  return fetch('./orders')
+    .then(res => {
+      if(res.statusCode !== 200 && res.statusCode < 300) {
+        return undefined
+      }
+      return res.json()
+    })
+    .catch(err => console.error(err))
+}
 
 const printOrders = () => {
   ++count
@@ -20,17 +21,19 @@ const printOrders = () => {
     .then(orders => {
       const items = orders.items
 
-      if(orders.pending) {
+      if(!items && orders.pending) {
         app.innerHTML = `<div style="text-align:center">Waiting: ${count}</div>`
         setTimeout(printOrders, 1000)
       } else {
+        app.innerHTML = `Total Items: ${Object.keys(items).length - 2}`
+
         Object.keys(items).forEach(key => {
           if(key !== 'expires' && key !== 'pending') {
-            const item = items[key]
-            output.push(`<div style="padding:0.5rem"><strong>${item.name}</strong> : Vol (${item.vol}) : Sell (${item.orders.sell.length}) : Buy (${item.orders.buy.length})</div>`)
+            const o = orders[key]
+            output.push(`<div style="padding:0.5rem"><strong>${o.name}</strong> : Vol (${o.mass}) : Sell (${o.orders.sell.length}) : Buy (${o.orders.buy.length})</div>`)
           }
         })
-        app.innerHTML = output.join('')
+        app.innerHTML = app.innerHTML + output.join('')
       }
     }).catch(err => console.error(err))
 }
